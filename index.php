7a47cc0b96007acc306b47b542a21fc80d4c73a6
rendydev404 <rendyakun50@gmail.com>
2026-08-12 14:31:29 +0700

    Bust login stylesheet cache

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Login - SIKS SMK Al Amin</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= filemtime(__DIR__ . '/assets/css/style.css') ?>">
 </head>
 <body>
     <div class="login-container">
